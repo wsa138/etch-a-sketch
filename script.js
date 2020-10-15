c4850b917f2f632c741cfd08bf35d52b2aeede3e
William Addesso <wsaddesso@gmail.com>
2020-10-14 23:45:28 -0400

    Created 16x16 grid of squares.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// A variable that creates a new div element.
-var square = document.createElement("div");
-
-// Sets the class of created div element.
-square.setAttribute("class", "square");
-
 // Variable containing grid container.
 var container = document.getElementById("grid-container");
-
-// Add div element to grid container.
-container.appendChild(square);
 
 // Variable contains total number of squares in the grid.
 var totalSquares = document.getElementById("grid-container").childElementCount;
 
+// Function creates the amount of squares specified in argument.
+function createGrid(totSquares) {
+    for (i = totSquares; i < 256; i++) {
+        var square = document.createElement("div");
+        square.setAttribute("class", "square");
+        container.appendChild(square);
+    }
+}
 
+createGrid(totalSquares);
