dd5ce86e6713f4403c948353dda039705935f8db
William Addesso <wsaddesso@gmail.com>
2020-10-20 02:06:54 -0400

    Added function that removes old grid and creates a new grid with a user designated size.

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ var container = document.getElementById("grid-container");
 var totalSquares = document.getElementById("grid-container").childElementCount;
 
 // Function creates the amount of squares specified in argument.
-function createGrid(totalChildren) {
-    for (i = totalChildren; i < 256; i++) {
+function createGrid(size = 16) {
+    for (i = 0; i < (size * size); i++) {
+        let gridContainer = document.getElementById("grid-container");
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         var square = document.createElement("div");
         square.setAttribute("class", "square");
         square.setAttribute("id", `square${i}`)
@@ -15,8 +18,8 @@ function createGrid(totalChildren) {
 }
 
 // Adds mouseover event to all squares by their id.
-function addSquareEvent() {
-    for (i = 0; i <256; i++) {
+function addSquareEvent(size = 16) {
+    for (i = 0; i < (size*size); i++) {
         let squareElement = document.getElementById("square" + i);
         squareElement.addEventListener("mouseover", function() {
             squareElement.style.backgroundColor = "black";
@@ -25,7 +28,7 @@ function addSquareEvent() {
 }
 
 // Calls the function that creates a the grid.
-createGrid(totalSquares);
+createGrid();
 
 // Calls functin to set events for mouseover.
 addSquareEvent();
@@ -36,9 +39,13 @@ var resetButton = document.getElementById("reset");
 // Variable contains reference to square class for all squares.
 var allSquares = Array.from(document.getElementsByClassName("square"));
 
-// Adds a click event listener to reset button that changes all square colors.
+/* Loops through and removes old squares from previous grid.
+Creates new grid. */
 resetButton.addEventListener("click", function() {
-    for (i = 0; i < 256; i++) {
-        allSquares[i].style.backgroundColor = "grey";
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
     }
+    let userSize = prompt("Enter the size of your grid", "16")
+    createGrid(userSize);
+    addSquareEvent(userSize);
 })
